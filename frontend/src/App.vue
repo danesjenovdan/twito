ef8f10c71fd0821bf62c0b3ec83e9bef09d2c2b0
mzgajner <mato@zgajner.com>
2020-10-31 18:24:44 +0100

    Load sample data from API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  {{ tweets }}
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { generateIntervals, calculateTweetTime } from "./timeAnalysis";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      tweets: [],
+    };
+  },
+  created() {
+    this.fetchTweetData();
+  },
+  methods: {
+    async fetchTweetData() {
+      try {
+        const response = await fetch(`${API_URL}2020-10-30`);
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+        const tweets = await response.json();
+        this.tweets = tweets;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
