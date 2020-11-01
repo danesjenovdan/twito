41077f971381fdf37199ab69730d244fdcd71843
muki <filipdobranic@gmail.com>
2020-11-01 10:43:02 +0100

    add simple in-memory cache

--- api/app.py
+++ api/app.py
@@ -1,17 +1,22 @@
 from flask import Flask, Response
 from flask_cors import CORS
+from flask_caching import Cache
 from requests import get
 from requests.auth import HTTPBasicAuth
 import json
 import csv
 import codecs
 from contextlib import closing
-from config import DMI_TCAT_USERNAME, DMI_TCAT_PASSWORD
+from config import DMI_TCAT_USERNAME, DMI_TCAT_PASSWORD, CACHE_CONFIG
 
 app = Flask(__name__)
 CORS(app)
 
+app.config.from_mapping(CACHE_CONFIG)
+cache = Cache(app)
+
 @app.route('/<date>', methods=['GET'])
+@cache.memoize()
 def index(date):
   file_url = f'http://51.15.220.60/analysis/mod.export_tweets.php?dataset=ONLY_jjansasds&query=&url_query=&media_url_query=&exclude=&from_user_name=&from_user_lang=&lang=&exclude_from_user_name=&from_user_description=&from_source=&startdate={date}&enddate={date}&whattodo=export_tweets&exportSettings=&graph_resolution=day&outputformat=csv'
   with closing(get(file_url, stream=True, auth=HTTPBasicAuth(DMI_TCAT_USERNAME, DMI_TCAT_PASSWORD))) as stream:
--- api/config.py
+++ api/config.py
@@ -5,3 +5,9 @@ load_dotenv()
 
 DMI_TCAT_USERNAME = os.getenv('DMI_TCAT_USERNAME')
 DMI_TCAT_PASSWORD = os.getenv('DMI_TCAT_PASSWORD')
+
+CACHE_CONFIG = {
+    'DEBUG': True,
+    'CACHE_TYPE': 'simple',
+    'CACHE_DEFAULT_TIMEOUT': 10 # change to 30 * 24 * 60 * 60 in production,
+}
