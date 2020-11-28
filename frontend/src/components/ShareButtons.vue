99a23e217a71ed62902893a38808e3a9659f378a
mzgajner <mato@zgajner.com>
2020-11-28 18:13:09 +0100

    Extract share buttons to own component

---
+++ frontend/src/components/ShareButtons.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="button">Prenesi</div>
+    <a :href="shareUrls.facebook" class="button">Deli na FB</a>
+    <a :href="shareUrls.twitter" class="button">Deli na TW</a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    date: { type: String, required: true },
+  },
+  computed: {
+    shareUrls() {
+      const text = encodeURIComponent('Maršal Twito.')
+      const url = encodeURIComponent(`https://twito.si/#/${this.date}`)
+      return {
+        facebook: `https://www.facebook.com/dialog/feed?app_id=301375193309601&redirect_uri=${url}&link=${url}&ref=responsive&name=${text}`,
+        twitter: `https://twitter.com/intent/tweet?text=${text}%20${url}`,
+      }
+    },
+  },
+})
+</script>
+
+<style scoped>
+.button {
+  color: inherit;
+  display: block;
+  text-decoration: none;
+  border: 1px solid white;
+  border-radius: 1.4rem;
+  height: 2.8rem;
+  line-height: 2.625rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+.button:not(:last-child) {
+  margin-bottom: 1rem;
+}
+.button:hover {
+  background: white;
+  color: black;
+}
+
+@media (min-width: 768px) {
+  .container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .button {
+    width: 11.625rem;
+    margin: 0 0.666rem !important;
+  }
+}
+</style>
--- frontend/src/components/TweetStats.vue
+++ frontend/src/components/TweetStats.vue
@@ -22,9 +22,7 @@
   </div>
 
   <div class="box-bottom button-container">
-    <div class="button">Prenesi</div>
-    <a :href="shareUrls.facebook" class="button">Deli na FB</a>
-    <a :href="shareUrls.twitter" class="button">Deli na TW</a>
+    <share-buttons :date="date" />
   </div>
 </template>
 
@@ -36,12 +34,14 @@ import { sl } from 'date-fns/locale'
 import { getTweetTime, getTweetCounts } from '../utils'
 import { fetchTweetData } from '../api'
 import BigTweetCounts from './BigTweetCounts.vue'
+import ShareButtons from './ShareButtons.vue'
 import SmallTweetCount from './SmallTweetCount.vue'
 import Timeline from './Timeline.vue'
 
 export default defineComponent({
   components: {
     BigTweetCounts,
+    ShareButtons,
     SmallTweetCount,
     Timeline,
   },
@@ -62,14 +62,6 @@ export default defineComponent({
     },
     formattedDate() {
       return format(new Date(this.date), 'EEEE, d. MMMM y', { locale: sl })
-    },
-    shareUrls() {
-      const text = encodeURIComponent('Maršal Twito.')
-      const url = encodeURIComponent(`https://twito.si/#/${this.date}`)
-      return {
-        facebook: `https://www.facebook.com/dialog/feed?app_id=301375193309601&redirect_uri=${url}&link=${url}&ref=responsive&name=${text}`,
-        twitter: `https://twitter.com/intent/tweet?text=${text}%20${url}`,
-      }
     },
   },
   watch: {
@@ -123,38 +115,5 @@ export default defineComponent({
   background: black;
   height: 1px;
   margin: 2.25rem 0 1.875rem;
-}
-
-.button {
-  color: inherit;
-  display: block;
-  text-decoration: none;
-  border: 1px solid white;
-  border-radius: 1.4rem;
-  height: 2.8rem;
-  line-height: 2.625rem;
-  font-size: 1.25rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  cursor: pointer;
-}
-.button:not(:last-child) {
-  margin-bottom: 1rem;
-}
-.button:hover {
-  background: white;
-  color: black;
-}
-
-@media (min-width: 768px) {
-  .button-container {
-    display: flex;
-    justify-content: center;
-  }
-
-  .button {
-    width: 11.625rem;
-    margin: 0 0.666rem !important;
-  }
 }
 </style>
