8bcb53aacfbad486c6a9a7247a1e01b8df1406bd
mzgajner <mato@zgajner.com>
2020-11-29 09:36:39 +0100

    Use correct title and description in index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,9 @@
+import _ from 'lodash'
+import templateStrings from './templateStrings.json'
+
 export default {
   optimizeDeps: {
     include: ['lodash-es/throttle', 'lodash-es/camelCase'],
   },
+  indexHtmlTransforms: [({ code }) => _.template(code)(templateStrings)],
 }
