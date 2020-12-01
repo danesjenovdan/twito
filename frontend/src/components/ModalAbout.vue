4b22dcd05b91c5fb20e51777f79d6ed1a4c65b69
muki <filipdobranic@gmail.com>
2020-12-01 15:27:11 +0100

    final tweaks befor v0.1 launch

--- frontend/src/components/ModalAbout.vue
+++ frontend/src/components/ModalAbout.vue
@@ -1,32 +1,40 @@
 <template>
   <modal title="O projektu">
     <p>
-      Premier Janez Janša rad tvita. Projekt Maršal Twito – Sledilnik beleži in
-      analizira njegovo aktivnost na Twitterju.
+      Premier Janez Janša veliko tvita. Projekt <i>Maršal Twito – Sledilnik</i> beleži
+      in analizira njegovo aktivnost na Twitterju.
     </p>
     <p>
-      Projekt želi pokazati na premierjevo zasvojenost z družabnimi omrežji.
-      Obenem želi opozoriti tudi na potrebo po večji transparentnosti in
-      nadzorom nad aktivnostmi najpomembnejšega človeka v državi, zlasti v času
-      epidemije.
+      Tvitanje je eden Janševih osrednjih političnih instrumentov. Kot predsednik vlade
+      državljanom in medijem na Twitterju sporoča pomembne odločitve, ukrepe in poteze.
+      Obenem pa ga uporablja za utrjevanje strankarske baze, mobilizacijo skrajne desnice
+      in dajanje potuhe sovražnemu govoru.
+    </p>
+    <p>
+      Videti je, da tvite večinoma piše sam, glede na njihovo pogostost (še posebno ritvitov) pa
+      je mogoče slutiti, da na omrežju preživlja veliko časa. Dokler je v funkciji predsednika
+      vlade in to še posebej v času epidemije, je njegova zasvojenost z družbenimi omrežji
+      zaskrbljujoča in pomembna za širšo družbo.
+    </p>
+    <p>
+      Projekt Maršal Twito – Sledilnik želi opozoriti na to stanje in z analizo podatkov
+      pridobiti vpogled v propagandistično aktivnost najbolj vplivnega Twitter profila v državi.
     </p>
 
     <div class="subtitle">Pridruži se</div>
     <p>
-      Sledilnik nameravamo razvijati tudi v prihodnosti in redno dodajati nove
-      analize. Če imaš znanja in veščine z analiziranjem in vizualizacijo
-      podatkov, pa tudi s področja front-end programiranja, nam piši na
-      <a href="mailto:vsi@danesjenovdan.si">vsi@danesjenovdan.si</a>.
+      Sledilnik nameravamo razvijati tudi v prihodnosti in redno dodajati nove analize. Če imaš
+      znanja in veščine z analiziranjem in vizualizacijo podatkov, pa tudi s področja front-end
+      programiranja, nam piši na <a href="mailto:vsi@danesjenovdan.si">vsi@danesjenovdan.si</a>.
     </p>
 
     <div class="subtitle">Avtorji</div>
     <p>
-      Za spletno mesto skrbi ekipa Danes je nov dan. Če želiš podpreti naše
-      delo, <a href="https://danesjenovdan.si/doniraj">doniraj</a>.
+      Za spletno mesto skrbi ekipa <a href="https://danesjenovdan.si/">Danes je nov dan</a>. Če
+      želiš podpreti naše delo, lahko <a href="https://danesjenovdan.si/doniraj">doniraš</a>.
     </p>
     <p>
-      Naslovno ilustracijo je ustvaril
-      <a href="https://www.stokto.com/">Stokto</a>.
+      Naslovno ilustracijo je ustvaril <a href="https://www.stokto.com/">Stokto</a>.
     </p>
   </modal>
 </template>
--- frontend/src/components/ModalMethodology.vue
+++ frontend/src/components/ModalMethodology.vue
@@ -1,22 +1,24 @@
 <template>
   <modal title="Kako računamo čas?">
     <p>
-      S sledilnikom na podlagi aktivnosti profila @jjansasds računamo oceno
-      preživetega časa na Twitterju. Interval časovne aktivnosti začnemo
-      beležiti ob prvem tvitu ali ritvitu. Če se v 5 minutah po zadnjem tvitu
-      ali retvitu ne zgodi nič, interval zaključimo, saj sklepamo, da se je
-      trenutna aktivnost končala.
+      S sledilnikom na podlagi aktivnosti Twitter profila
+      <a href="https://twitter.com/jjansasds" target="_blank">@jjansasds</a> računamo
+      oceno preživetega časa na Twitterju. Dolžino intervala aktivnosti začnemo
+      beležiti, ko profil objavi tvit ali ritvit, in jo merimo do trenutka, ko po
+      tvitu ali ritvitu več kot 5 minut ni nove objave. Takrat sklepamo, da je Twitter
+      aktivnost zaključena. Dokler Maršal objavi vsaj en tvit na 5 minut, potem
+      domnevamo, da je njegova pozornost osredotočena zgolj na Twitter.
     </p>
 
     <p>
-      Trajanje posameznih intervalov nato seštejemo, da dobimo oceno dnevno
-      preživetega časa na Twitterju.
+      Trajanje posameznih intervalov v dnevu nato seštejemo, da dobimo skupno
+      oceno dnevno preživetega časa na Twitterju.
     </p>
 
     <p>
-      V intervalih ne upoštevamo premierjevih drugih aktivnosti, kot so
-      všečkanje tvitov, saj jih je težje beležiti. Če bi nam rad/-a pomagal/-a
-      rešiti to težavo, nam <a href="mailto:vsi@danesjenovdan.si">piši</a>.
+      V intervalih ne upoštevamo premierjevih drugih aktivnosti, kot so všečkanje
+      tvitov, saj jih je težje beležiti. Če bi nam rad/-a pomagal/-a rešiti to težavo,
+      <a href="mailto:vsi@danesjenovdan.si" target="_blank">nam piši</a>.
     </p>
   </modal>
 </template>
