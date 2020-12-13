62699db80983429f955017c628aaa087f3b617b5
mzgajner <mato@zgajner.com>
2020-12-13 14:14:10 +0100

    Generate date cache key with period for tomorrow's dates

--- api/utils.py
+++ api/utils.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 def calculate_date_cache_key(date):
   logger.info(f'Calculating cache key for {date}')
-  if datetime.now().date() == datetime.strptime(date, '%Y-%m-%d').date():
+  if datetime.now().date() <= datetime.strptime(date, '%Y-%m-%d').date():
     period = int(int(datetime.strftime(datetime.now(), '%M')) / 15)
     calculated_cache_key = datetime.strftime(datetime.now(), f'%Y-%m-%d %H {period}')
   else:
