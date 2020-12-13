229c5fcddd8b9b3bafd37c4f7d4fff45d7609737
mzgajner <mato@zgajner.com>
2020-12-13 13:53:54 +0100

    Configure flask logging to work with python logger

--- api/config.py
+++ api/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from logging.config import dictConfig
 
 load_dotenv()
 
@@ -11,3 +12,19 @@ CACHE_CONFIG = {
     'CACHE_TYPE': 'simple',
     'CACHE_DEFAULT_TIMEOUT': 30 * 24 * 60 * 60, # 30 days
 }
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
