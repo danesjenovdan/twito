418031c4ec5c6913972bbc0064c873a2cf4d7a65
mzgajner <mato@zgajner.com>
2020-12-20 13:12:08 +0100

    Take utc-cet offset into account when fetching from DMI-TCAT

--- api/dmi_tcat.py
+++ api/dmi_tcat.py
@@ -1,23 +1,37 @@
 import csv
 import codecs
+from datetime import datetime, timedelta, timezone
 
 from requests.auth import HTTPBasicAuth
 from contextlib import closing
 from requests import get
 
 from config import DMI_TCAT_USERNAME, DMI_TCAT_PASSWORD
+import slovenian_time
+
 
 def fetch_tweets_for_date(start, end=None):
   if not end:
     end = start
 
-  file_url = f'http://51.15.220.60/analysis/mod.export_tweets.php?dataset=ONLY_jjansasds&query=&url_query=&media_url_query=&exclude=&from_user_name=&from_user_lang=&lang=&exclude_from_user_name=&from_user_description=&from_source=&startdate={start}&enddate={end}&whattodo=export_tweets&exportSettings=&graph_resolution=day&outputformat=csv'
+  slo_start = slovenian_time.start_of_day(start)
+  slo_end = slovenian_time.end_of_day(end)
+
+  # DMI-TCAT query params are UTC, so we need to fetch an additional day as last
+  # one/two hours of the same day in UTC are already in the next day in CET.
+  day_before_start = datetime.fromisoformat(start) - timedelta(days=1)
+  start_query = day_before_start.strftime('%Y-%m-%d')
+  end_query = end
+
+  file_url = f'http://51.15.220.60/analysis/mod.export_tweets.php?dataset=ONLY_jjansasds&query=&url_query=&media_url_query=&exclude=&from_user_name=&from_user_lang=&lang=&exclude_from_user_name=&from_user_description=&from_source=&startdate={start_query}&enddate={end_query}&whattodo=export_tweets&exportSettings=&graph_resolution=day&outputformat=csv'
   with closing(get(file_url, stream=True, auth=HTTPBasicAuth(DMI_TCAT_USERNAME, DMI_TCAT_PASSWORD))) as stream:
     csv_reader = csv.DictReader(
       codecs.iterdecode(stream.iter_lines(), 'utf-8')
     )
     tweets = []
     for row in csv_reader:
-      tweets.append(row)
+      tweet_time = datetime.fromisoformat(row["created_at"]).replace(tzinfo=timezone.utc)
+      if slo_start < tweet_time < slo_end:
+        tweets.append(row)
 
   return tweets
