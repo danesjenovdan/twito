75694eb2f1feff8c9e5340c36444bda23227ac4f
mzgajner <mato@zgajner.com>
2020-12-23 18:48:20 +0100

    Correctly pass env variables

--- api/config.py
+++ api/config.py
@@ -1,8 +1,5 @@
 import os
-from dotenv import load_dotenv
 from logging.config import dictConfig
-
-load_dotenv()
 
 DMI_TCAT_USERNAME = os.getenv('DMI_TCAT_USERNAME')
 DMI_TCAT_PASSWORD = os.getenv('DMI_TCAT_PASSWORD')
