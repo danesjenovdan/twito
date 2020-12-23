3a24c1739e00c34b6199af7958c9fd5d5a90d622
mzgajner <mato@zgajner.com>
2020-12-23 19:11:41 +0100

    Validate date param before running any other code

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_caching import Cache
 from config import CACHE_CONFIG
 
-from utils import SummaryCacheInfo, DateCacheInfo
+from utils import is_valid_date, SummaryCacheInfo, DateCacheInfo
 from tweets import get_summary_date_range, get_gap_date_range, group_by_day, get_all_calculations, get_gaps, get_hashtags, get_start_of_day
 from dmi_tcat import fetch_tweets_for_date
 
@@ -13,21 +13,6 @@ CORS(app)
 app.config.from_mapping(CACHE_CONFIG)
 cache = Cache(app)
 
-
-@app.route('/favicon.ico', methods=['GET'])
-@cache.cached()
-def favicon():
-  abort(404)
-
-@app.route('/<date>', methods=['GET'])
-@cache.cached(forced_update=DateCacheInfo.should_force_update)
-def index(date):
-  tweets = fetch_tweets_for_date(date)
-  calculations = get_all_calculations(tweets)
-  hashtags = get_hashtags(tweets)
-  start_of_day = get_start_of_day(date)
-
-  return jsonify(tweets=tweets, calculations=calculations, hashtags=hashtags, start_of_day=start_of_day)
 
 @app.route('/summary', methods=['GET'])
 @cache.cached(forced_update=SummaryCacheInfo.should_force_update)
@@ -51,3 +36,17 @@ def running_gap():
   gaps = get_gaps(tweets)
 
   return jsonify(gaps)
+
+@app.route('/', defaults={'date': ''})
+@app.route('/<date>', methods=['GET'])
+@cache.cached(forced_update=DateCacheInfo.should_force_update)
+def index(date):
+  if not is_valid_date(date):
+    abort(404)
+
+  tweets = fetch_tweets_for_date(date)
+  calculations = get_all_calculations(tweets)
+  hashtags = get_hashtags(tweets)
+  start_of_day = get_start_of_day(date)
+
+  return jsonify(tweets=tweets, calculations=calculations, hashtags=hashtags, start_of_day=start_of_day)
