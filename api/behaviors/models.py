91ed21fe95217761faff13d93d17ad4dfc5ee5cb
Filip Muki Dobranić <filipdobranic@gmail.com>
2021-01-17 17:40:14 +0100

    remove rate limit argument from Timestampable.touch

--- api/behaviors/models.py
+++ api/behaviors/models.py
@@ -4,7 +4,7 @@ class Timestampable(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     updated_at = models.DateTimeField(auto_now=True, null=False)
 
-    def touch(self, rate_limit_in_seconds=None):
+    def touch(self):
         self.save(update_fields=['updated_at'])
 
     class Meta:
